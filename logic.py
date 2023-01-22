a626aaab6a6b6cede7d7ee1c12fca5593e58906f
GrigoriyBaranchuk <grigoriy.baranchuk@icloud.com>
2023-01-23 01:25:48 +0300

    Fix bug with data_now and time_now - they returns data and time of server not of users.

--- logic.py
+++ logic.py
@@ -1,15 +1,17 @@
 from datetime import datetime
+import pytz
 from telebot import types
 
 
 def now_time():
     """return time at the current moment in str"""
-    return datetime.now().strftime('%H:%M')
+    return datetime.now(pytz.timezone('Europe/Moscow')).strftime('%H:%M')
 
 
 def now_date():
     """return time at the current moment in str"""
-    return datetime.now().strftime('%d-%m-%Y')
+    return datetime.now(pytz.timezone('Europe/Moscow')).strftime('%d-%m-%Y')
+# TODO change function  to create tim of user not server or some utc
 
 
 def add_answers(bot, message):
